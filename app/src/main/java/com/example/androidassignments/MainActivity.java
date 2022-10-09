f3db0029161eca1158c37e2d4f1a74d01de13fbe
zancrash <farzanali1296@gmail.com>
2022-10-09 18:30:45 -0400

    fix id reference

--- app/src/main/java/com/example/androidassignments/MainActivity.java
+++ app/src/main/java/com/example/androidassignments/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button loginBtn = (Button)findViewById(R.id.loginbtn);
+        Button loginBtn = (Button)findViewById(R.id.button);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
